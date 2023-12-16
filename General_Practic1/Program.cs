94384ebaf0b544405e68fec0b89ca50137578fcd
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-12-16 16:03:36 +0300

    практичный код

--- General_Practic1/Program.cs
+++ General_Practic1/Program.cs
@@ -6,46 +6,31 @@ class Program
 {
     static HttpClient client = new HttpClient();
     static async Task Main()
-    {
-        System.Console.WriteLine("Input name site");
-        UriBuilder build = new UriBuilder();
-        build.Host = Console.ReadLine();
-        build.Scheme = "https";
-
-        Uri uri = build.Uri;
-
-        using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri.OriginalString))
+    {   
+        System.Console.WriteLine("Input page name: ");
+        Uri uri = new UriBuilder("https://",Console.ReadLine()).Uri;
+        using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri))
         {
             try 
             {
                 HttpResponseMessage response = await client.SendAsync(request);
+                System.Console.WriteLine("status: " + response.ReasonPhrase);
+                if (response.ReasonPhrase != "OK") return;
 
-                if (response.ReasonPhrase != "OK")  
+                foreach (var header in response.Headers)
                 {
-                    System.Console.WriteLine("error status code: " + response.ReasonPhrase);
-
-                }
-                System.Console.WriteLine("name status code: " + response.ReasonPhrase);
-                HttpHeaders headers = response.Headers;
-                foreach (KeyValuePair<string,IEnumerable<string>> pair in headers)
-                {
-                    System.Console.WriteLine(pair.Key + ":\t");
-                    foreach (string value in pair.Value) 
+                    System.Console.WriteLine(header.Key + ": ");
+                    foreach (var value in header.Value)
                     {
                         System.Console.WriteLine(value);
                     }
                 }
-                System.Console.WriteLine("\nContent:");
-                HttpContent content = response.Content;
-                string contentText = await content.ReadAsStringAsync();
-                System.Console.WriteLine(contentText.Substring(0, 2000));
+                System.Console.WriteLine((await response.Content.ReadAsStringAsync()).Substring(0,1000));
             }
-            catch (Exception e) 
+            catch (Exception e)
             {
                 System.Console.WriteLine(e.Message);
             }
         }
-        
-
     }
 }
