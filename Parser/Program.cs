a823850d76eddf9efb255f5d3f79ca6dee071f2a
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-12-18 15:32:54 +0300

    Add files via upload

---
+++ Parser/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Program
+{
+    using System.Reflection.Metadata;
+    using HtmlAgilityPack;
+    class Program
+    {
+        static async Task Main()
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load("https://vc.ru/dev/148017-parsing-na-c-s-htmlagilitypack");
+            HtmlNodeCollection collection = doc.GetElementbyId("id").ChildNodes;
+            
+        }   
+    }
+}
