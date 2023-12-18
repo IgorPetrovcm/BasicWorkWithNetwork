894b17828385797b6f215821c091bc4ce04995f4
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-12-18 18:19:39 +0300

    HtmlDocument  через текст html

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -2,14 +2,44 @@
 {
     using System.Reflection.Metadata;
     using HtmlAgilityPack;
+    using System.Net.Http;
+    
+
     class Program
     {
-        static async Task Main()
+        private static HttpClient client = new HttpClient();
+        static void Main()
         {
-            HtmlDocument doc = new HtmlDocument();
-            doc.Load("https://vc.ru/dev/148017-parsing-na-c-s-htmlagilitypack");
-            HtmlNodeCollection collection = doc.GetElementbyId("id").ChildNodes;
+            /*HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, new Uri("https://github.com"));
+            HttpResponseMessage resp = client.SendAsync(request).Result;
+            Console.WriteLine(resp.Content.ReadAsStringAsync().Result);*/
             
-        }   
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html: GetHtmlDocument(url: new Uri("https://google.com")).ToString());
+            HtmlNode htmlBody = doc.DocumentNode.SelectSingleNode("//body");
+            Console.WriteLine(htmlBody.OuterHtml);
+        }
+
+        static async Task<string> GetHtmlDocument(Uri url)
+        {
+            if (url == null)
+            {
+                throw new ArgumentNullException(nameof(url));
+            }
+            try
+            {
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+                HttpResponseMessage resp = client.SendAsync(request).Result;
+                return resp.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.Data);
+                Console.WriteLine(ex.Message);
+            }
+
+            throw new Exception();
+        }
+        
     }
 }
